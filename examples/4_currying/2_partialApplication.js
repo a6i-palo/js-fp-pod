e9fac8d28436ebfedcb331343dd3183bba4a5c8a
Xavier Xu <xxu@palo-it.com>
2021-02-03 20:53:06 +0800

    Add curriedBuildUrl implementation in and fix twitterFavion1

--- examples/4_currying/2_partialApplication.js
+++ examples/4_currying/2_partialApplication.js
@@ -4,7 +4,11 @@ const multiplyWith2Argument = multiply(1)(2); // Returns a function
 const multiplyWith3Argument = multiplyWith2Argument(3); // 6
 
 // TODO: create a function curriedBuildUrl
+const curriedBuildUrl = (scheme) => (domain) => (path) =>
+  scheme + "://" + domain + "/" + path;
 const twitterFavicon = curriedBuildUrl("https")("favicon.ico")("twitter.com");
+console.log(twitterFavicon);
 
 const buildHttpsUri = curriedBuildUrl("https");
-const twitterFavicon1 = buildHttpsUri("twitter.com", "favicon.ico");
+const twitterFavicon1 = buildHttpsUri("twitter.com")("favicon.ico");
+console.log(twitterFavicon1);
