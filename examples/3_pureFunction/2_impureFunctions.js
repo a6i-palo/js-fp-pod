f00a4b921ccd77da786c3edee21246e9bd8c8088
Ziwei <zang@palo-it.com>
2021-02-17 00:50:08 +0800

    Drop examples folder

--- examples/3_pureFunction/2_impureFunctions.js
+++
@@ -1,7 +0,0 @@
-// Impure function: output is different when you run with same input
-// rely on other information other than what you passed in
-const randomNum = Math.random();
-console.log(randomNum);
-
-const time = () => new Date().toLocaleTimeString();
-console.log(time());
--- examples/6_functor/1_map.js
+++
@@ -1,3 +0,0 @@
-// Example 1
-const functor = [0, 1, 2, 3, 4, 5, 6].map((x) => x + 1);
-console.log(functor);
